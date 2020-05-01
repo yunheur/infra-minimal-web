a1844f26d592628413f93c8380cff8b2d55470b8
yunheur <yunheur@gmail.com>
2020-05-01 13:50:34 +0000

    modify certbot.renew.sh

--- certbot.renew.sh
+++ certbot.renew.sh
@@ -1,4 +1,4 @@
-ocker run -it --rm --name certbot \
+docker run -it --rm --name certbot \
   -v '/etc/letsencrypt:/etc/letsencrypt' \
   -v '/var/lib/letsencrypt:/var/lib/letsencrypt' \
   certbot/certbot renew --manual --preferred-challenges dns --server https://acme-v02.api.letsencrypt.org/directory
